23988f2004dd6d238b63896fd0c7497b2d7ceb8a
BobMuscarella <bob.muscarella@gmail.com>
2024-04-11 16:26:19 +0200

    Add scripts

---
+++ Scripts/2.Matching-edna-stem-data.R
@@ -0,0 +1,213 @@
+library(phyloseq)
+library(googlesheets4)
+library(EDIutils)
+
+### Load data from Glenn
+load("Raw_data/CescPRdata.RData")
+
+### Load link between OTUs and RefIDs
+otu.potu.link <- readRDS("Raw_data/Reference_library_filtering/OTU-to-RefIDs-List.rds")
+
+### Fix the MANBID/SLOBER mixup
+otu.potu.link[[6]]$abundance[otu.potu.link[[6]]$genus %in% "Manilkara"] <- "POTU66"
+otu.potu.link[[6]]$abundance[otu.potu.link[[6]]$Genus %in% "POTU66"] <- "POTU56"
+### DELETE PSYBER and RONPOR
+otu.potu.link[[6]] <- otu.potu.link[[6]][!otu.potu.link[[6]]$abundance %in% "POTU72",]
+otu.potu.link[[6]] <- otu.potu.link[[6]][!otu.potu.link[[6]]$abundance %in% "POTU75",]
+
+### (For now) subset the main eDNA data to use
+# d <- R3phylo.plants.tax[[5]]
+d <- R1ref.lib.list[[6]]
+
+### Prune phyloseq object to the OTUs that appear in the soil data AND correspond to the reference library
+d <- prune_taxa(otu.potu.link[[6]]$OTU, d)
+
+### Load the LFDP 2023 census extract data
+tree <- readRDS("Raw_data/LFDP2023-extract-20240327.RDA")
+
+# list of species codes observed in tree data
+# writexl::write_xlsx(data.frame(code=colnames(tree$abund$`100`)), "Raw_data/LFDP2023-codes.xlsx")
+
+# Access raw data from 2016 census
+# census6 <-  readr::read_csv(file=EDIutils::read_data_entity(packageId = "knb-lter-luq.119.1545979", 
+#                                    entityId = "325c43057e0dd4e1cd6a13fa5125a76d"))
+# Species 
+# sort(unique(census6$Mnemonic[!census6$Mnemonic %in% colnames(tree$abund$`100`)]))
+
+### Load LFDP species codes
+# codes_raw <- EDIutils::read_data_entity(packageId = "knb-lter-luq.118.215602", 
+#                                    entityId = "cffeaeca64642878797cbb1621fcf07e")
+# codes <- readr::read_csv(file = codes_raw)
+
+# # Read in the leaf OTUs googlesheet
+# leaf.otus <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Ihe1dXkENK4zZ0lBsN1_1tDnaIvVD3u9oiNHjtMRHzo/edit?usp=sharing", sheet="Leaf OTUs")
+# 
+# leaf.otus$code <- substring(leaf.otus$mnemonic, 1, 6)
+# 
+# codes$POTU <- leaf.otus$'FASTA#'[match(codes$`SPECIES CODE`, leaf.otus$code)]
+# 
+# writexl::write_xlsx(codes, "Raw_data/LFDP-SPcodes.xlsx")
+
+codes <- readxl::read_xlsx("Raw_data/LFDP-SPcodes.xlsx")
+codes <- codes[codes$LFDP2023==1,]
+
+### Load the sample point coordinates
+sample_xy <- read.csv("Raw_data/LFDP-eDNA-xy.csv")
+
+### Sample "C24_P2" (a "normal" sampling location) is not missing from soil data.
+### Glen confirmed that this was dropped due to 'bad' eDNA data
+
+### Prune phyloseq object to the 39 sample points in the '40 point analysis'
+d <- prune_samples(grepl("normal", d@sam_data$factorlevel), d)
+
+# Get coordinates from 39 sample points included
+xy <- d@sam_data[grepl("normal", d@sam_data$factorlevel),c("X","Y")]
+
+### Make the rows of the stem data match the rows of the eDNA otu-table data
+## *THIS FIRST BLOCK WAS TO JUST TO EITHER ABUNDANCE ONLY, NEW BLOCKS BELOW DO ABUND, BA, AND NN
+# stem <- lapply(tree$abund, function(x) x[match(paste(xy$X, xy$Y), paste(sample_xy$PX, sample_xy$PY)),])
+# for(i in seq_along(stem)){
+#   rownames(stem[[i]]) <- rownames(d@otu_table)
+# }
+
+stem_abund <- lapply(tree$abund, function(x) {
+  rbind(x[match(paste(xy$X, xy$Y), paste(sample_xy$PX, sample_xy$PY)),],
+        x[88:1087,])})
+for(i in seq_along(stem_abund)){
+  rownames(stem_abund[[i]]) <- c(rownames(d@otu_table), paste0('random',1:1000))
+}
+
+
+stem_ba <- lapply(tree$ba, function(x) {
+  rbind(x[match(paste(xy$X, xy$Y), paste(sample_xy$PX, sample_xy$PY)),],
+        x[88:1087,])})
+for(i in seq_along(stem_ba)){
+  rownames(stem_ba[[i]]) <- c(rownames(d@otu_table), paste0('random',1:1000))
+}
+
+stem_nn <- rbind(tree$nearest_sp[match(paste(xy$X, xy$Y),
+                                        paste(sample_xy$PX, sample_xy$PY)),], tree$nearest_sp[88:1087,])
+rownames(stem_nn) <- c(rownames(d@otu_table), paste0('random',1:1000))
+
+stem <- list(abund=stem_abund, ba=stem_ba, nearest_sp=stem_nn)
+
+
+
+
+
+##### THIS BLOCK COLLAPSES THE DNA DATA TO THE gOTU CLUSTERS
+### Identify all POTUs/OTUs we need to keep because they are assigned to a gOTU
+potus.keep <- codes$POTU[!is.na(codes$gOTU)]
+otus.keep <- otu.potu.link[[6]]$OTU[match(potus.keep, otu.potu.link[[6]]$abundance)]
+gotus.keep <- codes$gOTU[!is.na(codes$gOTU)]
+codes.keep <- codes$`SPECIES CODE`[!is.na(codes$gOTU)]
+
+# POTUs that we keep and to be collapsed
+codes.collapse.list <- split(codes.keep, gotus.keep)
+otus.collapse.list <- split(otus.keep, gotus.keep)
+
+# loop to collapse taxa in phyloseq objects
+for(i in seq_along(otus.collapse.list)){
+  if(length(unique(otus.collapse.list[[i]][!is.na(otus.collapse.list[[i]])])) > 1){
+    taxcollapse <- unique(otus.collapse.list[[i]][!is.na(otus.collapse.list[[i]])])
+    taxcollapse <- taxcollapse[taxcollapse %in% rownames(d@tax_table)]
+    d <- merge_taxa(d, taxcollapse, 1)
+  }
+}
+
+##### THIS BLOCK COLLAPSES THE STEM DATA TO THE gOTU CLUSTERS
+stem.otu <- list()
+for(r in seq_along(stem$abund)){
+  
+  # Initialize a matrix to hold collapsed results for a given radius
+  tmat_abund <- matrix(ncol=length(codes.collapse.list), nrow=1039)
+  tmat_ba <- matrix(ncol=length(codes.collapse.list), nrow=1039)
+  tmat_nn <- matrix(ncol=length(codes.collapse.list), nrow=1039)
+  
+  # loop to collapse taxa in phyloseq objects
+  for(i in seq_along(codes.collapse.list)){
+    tmp_abund <- stem$abund[[r]][,which(colnames(stem$abund[[1]]) %in% codes.collapse.list[[i]])]
+    tmp_ba <- stem$ba[[r]][,which(colnames(stem$ba[[1]]) %in% codes.collapse.list[[i]])]
+    tmp_nn <- stem$nearest_sp[,which(colnames(stem$nearest_sp) %in% codes.collapse.list[[i]])]
+    
+    if(is.matrix(tmp_abund)){
+      tmat_abund[,i] <- rowSums(tmp_abund)
+      tmat_ba[,i] <- rowSums(tmp_ba)
+      tmat_nn[,i] <- rowSums(tmp_nn)
+    } else {
+      tmat_abund[,i] <- tmp_abund
+      tmat_ba[,i] <- tmp_ba
+      tmat_nn[,i] <- tmp_nn
+    }
+  }
+  
+  tmat_abund <- as.data.frame(tmat_abund)
+  tmat_ba <- as.data.frame(tmat_ba)
+  tmat_nn <- as.data.frame(tmat_nn)
+  
+  names(tmat_nn) <- names(tmat_ba) <- names(tmat_abund) <- names(codes.collapse.list)
+  rownames(tmat_nn) <- rownames(tmat_ba) <- rownames(tmat_abund) <- rownames(stem$abund[[1]])
+  
+  stem.otu$abund[[r]] <- tmat_abund
+  stem.otu$ba[[r]] <- tmat_ba
+  stem.otu$nn <- tmat_nn
+  
+  names(stem.otu$ba)[r] <- names(stem.otu$abund)[r] <- names(stem$abund)[r]
+}
+
+
+
+
+### To make the columns match, and add zero columns to OTU data
+
+# The new phyloseq object should have these OTUs as colnames
+collapsed.otus <- unlist(lapply(otus.collapse.list, function(x) x[!is.na(x)][1]))
+
+# The stem data has these names exactly
+all(names(collapsed.otus) == names(stem.otu$abund[[1]]))
+
+# Some gOTU names are in the soil data but not the collapsed list because they aren't trees
+# ** NEED TO CHECK THIS AGAIN AFTER GLENN REDOES THINGS B/C SOMETHING IS WITH e.g. POTU66
+qs <- colnames(d@otu_table)[!colnames(d@otu_table) %in% collapsed.otus]
+otu.potu.link[[6]][otu.potu.link[[6]]$OTU %in% qs,]
+
+# Drop these from the OTU Table
+d <- prune_taxa(colnames(d@otu_table)[colnames(d@otu_table) %in% collapsed.otus], d)
+
+# Some gOTU names are not in the soil data (not detected).  For these we add a zero column.
+absent.gOTUs <- collapsed.otus[!collapsed.otus %in% colnames(d@otu_table)]
+
+# Make the tables comparable
+dnamat <- d@otu_table
+colnames(dnamat) <- names(collapsed.otus)[match(colnames(dnamat), collapsed.otus)]
+dnamat <- dnamat[,match(colnames(stem.otu$abund[[1]]), colnames(dnamat))]
+colnames(dnamat) <- colnames(stem.otu$abund[[1]])
+dnamat <- as.data.frame(dnamat)
+dnamat[is.na(dnamat)] <- 0
+
+
+#################################
+### ADD TRAIT DATA
+#################################
+trait_data <- read.csv("Raw_data/Fixed_traits.csv", row.names=1)
+traits <- matrix(nrow=length(codes.collapse.list), ncol=7)
+colnames(traits) <- colnames(trait_data)
+rownames(traits) <- names(codes.collapse.list)
+for(i in seq_along(codes.collapse.list)){
+  tmp <- trait_data[rownames(trait_data) %in% codes.collapse.list[[i]],]
+  if(nrow(tmp) > 0){ traits[i,] <- colMeans(tmp) } 
+}
+traits <- as.data.frame(traits)
+
+
+
+#################################
+### SAVE DATA FOR DOWNSTREAM ANALYSES
+#################################
+
+saveRDS(list(dnamat=dnamat, stem.otu=stem.otu, traits=traits), 
+        "Processed_data/stem-soil-39pt-data-20240411.RDA")
+
+
+
+
---
+++ Scripts/3.LFDP-Analysis.R
@@ -0,0 +1,432 @@
+### Load libraries
+library(vegan)
+library(caret)
+library(compositions)
+library(viridis)
+library(ecodist)
+
+### Load summary data
+dnamat <- readRDS("Processed_data/stem-soil-39pt-data-20240411.RDA")[[1]]
+stem.otu <- readRDS("Processed_data/stem-soil-39pt-data-20240411.RDA")[[2]]
+traits <- readRDS("Processed_data/stem-soil-39pt-data-20240411.RDA")[[3]]
+
+### Make a DNA presence absence matrix
+dnamat.pa <- 1*(dnamat>0)
+
+### Color palette for plotting
+cp <- rev(viridis(20))
+
+
+
+##################
+### NOTES
+##################
+
+# Some 'first pass' exploratory analyses are below, separated by code blocks
+# The order is meaningless
+
+# SOME IDEAS OF THINGS TO DO
+
+# - rank abundance of stem vs. dna data
+
+# - Is there a land-use signature? 
+#     - Pool sites by land use and look at summary stats
+#     - PCA on DNA data and then look at land-use, soil type
+
+
+
+
+########################################################
+### Mantel test of stem distance matrix and dna distance matrix
+########################################################
+
+# Lots to consider about how to transform the data and which distance metric to use...
+
+# dnamat.dist <- vegdist(clr(dnamat), method='euclidean')
+dnamat.dist <- vegdist(dnamat.pa, method='raup')
+# dnamat.dist <- vegdist(dnamat.pa[,colSums(dnamat.pa)>0], method='raup')
+
+manres <- matrix(nrow=20, ncol=6)
+for(r in 1:20){
+  stem.dist <- vegdist(stem.otu$ba[[r]][1:39,], method='raup')
+  # stem.dist <- vegdist(decostand(stem.otu$ba[[r]][1:39,], "total", 1), method='jaccard')
+  manres[r,] <- round(ecodist::mantel(dnamat.dist ~ stem.dist), 3)
+}
+colnames(manres) <- names(ecodist::mantel(dnamat.dist ~ stem.dist))
+rownames(manres) <- seq(5, 100, 5)
+manres <- as.data.frame(manres)
+
+plot(rownames(manres), manres$mantelr, ylim=range(manres[,5:6]), pch=16, 
+     xlab="Radius (m)", ylab="Mantel correlation")
+segments(seq(5, 100, 5), manres$`llim.2.5%`, seq(5, 100, 5), manres$`ulim.97.5%`)
+abline(h=0, lty=2)
+
+
+########################################################
+### Procrusties test of stem ordination and dna ordinatino
+########################################################
+
+# Similar to Mantel test, lots to consider in terms of transformation, dist metric, ordination...
+
+prores <- list()
+
+for(r in 1:20){
+  
+  dnaord <- metaMDS(dnamat.pa, dist="raup")
+  # dnaord <- metaMDS(clr(dnamat), dist='euclidean')
+  
+  stemord <- metaMDS(vegdist(stem.otu$ba[[r]][1:39,], method='raup'))
+  # stemord <- metaMDS(stem.otu$ba[[r]][1:39,])
+                       
+  prores[[r]] <- protest(stemord, dnaord)
+}
+
+plot(sapply(prores, function(x) x$signif), ylim=c(0,1))
+abline(h=c(0.05, 0.95))
+
+
+########################################################
+### Whole plot rank abundance vs rank abundance of DNA data
+########################################################
+
+# "Whole plot" stem rank abundance and rank DNA read abundance is similar (~0.5) across spatial scales
+# This *should* be based on the sum total stems for the full plot 
+# but we don't have that for 2023 census so this uses sum of stems in our summary data
+
+est <- matrix(nrow=20, ncol=3)
+
+for(r in 1:20){
+  focdat <- stem.otu$abund[[r]]
+  res <- cor.test(rank(colSums(focdat)), rank(colSums(dnamat)))
+  est[r,] <- c(res$estimate, res$conf.int)
+}
+
+plot(est[,1], ylim=c(-1,1))
+segments(1:20, est[,2], 1:20, est[,3])
+abline(h=0, lty=2)
+
+
+
+########################################################
+### COMPUTE BALANCED ACCURACY 
+### (INCLUDING STANDARDIZED EFFECT SIZE OF BALANCED ACCURACY)
+### **Note that this takes ~30 min to run on Bob's computer!!!**
+########################################################
+
+balaccuracy.obs <- matrix(nrow=1000, ncol=20)
+balaccuracy.ses <- matrix(nrow=39, ncol=20)
+
+for(r in 1:20){
+  message(paste("radius =", r))
+  
+  for(site in 1:39){
+    
+    confus.obs <- confusionMatrix(table(dnamat.pa[site,], 
+                                    1*(stem.otu$abund[[r]][site,] > 0)))
+    balaccuracy.obs[site,r] <- (confus.obs$byClass[1] + confus.obs$byClass[2])/2
+    
+    balaccuracy.rand <- vector()
+    
+    for(s in 1:1000){
+      confus.rand <- confusionMatrix(table(dnamat.pa[site,], 
+                                           1*(stem.otu$abund[[r]][-c(1:39),] > 0)[s,]))
+      
+      balaccuracy.rand[s] <- (confus.rand$byClass[1] + confus.rand$byClass[2])/2
+    }
+    
+    balaccuracy.ses[site,r] <- (balaccuracy.obs[site,r] - mean(balaccuracy.rand)) / sd(balaccuracy.rand)
+  }
+}
+
+# Plot the results
+boxplot(balaccuracy.ses, main="SES of Balanced Accuracy", 
+        xlab="Radius (m)", axes=F, col=cp)
+polygon(c(-1,200,200,-1), c(-1.96,-1.96, 1.96, 1.96), col='grey', lty=2)
+boxplot(balaccuracy.ses, col=cp, add=T, axes=F)
+axis(1, labels=names(stem.otu$abund), at=1:20)
+axis(2)
+box()
+
+# Line plot (probably not very good)
+plot(c(0,20), range(balaccuracy.ses), pch=NA, axes=F)
+axis(1, labels=names(stem.otu$abund), at=1:20)
+axis(2)
+box()
+polygon(c(-1,200,200,-1), c(-1.96,-1.96, 1.96, 1.96), col='grey', lty=2)
+for(i in 1:39){
+  lines(1:20, balaccuracy.ses[i,], col=plasma(39)[i])
+}
+
+
+
+########################################################
+## COMMUNITY DISSIMILIARITY ANALYSIS
+########################################################
+
+## Gives warnings with 'raup' because there are 'empty' species
+res <- matrix(nrow=39, ncol=20)
+
+# Loop through spatial radii
+for(r in 1:20){
+  # Remove taxa with all zeros in both soil and stem data for this radius
+  tmp <- rbind(dnamat, stem.otu$abund[[r]][1:39,])
+  tmp <- tmp[,which(colSums(tmp)!=0)]
+  # Compute the index
+  tmpres <- as.matrix(vegdist(tmp>0, method="raup", binary = TRUE))
+  tmpres <- tmpres[1:39, 40:ncol(tmpres)]
+  res[,r] <- diag(tmpres)
+}
+
+boxplot(res, ylab="Raup Dissimilarity", xlab="Radius (m)", axes=F)
+axis(1, labels=names(stem.otu$abund), at=1:20)
+axis(2)
+
+
+### Dissimilarity using Chase et al. 2011 null model and Raup 
+### NOTE: Different null models (r1, r0) give different responses
+res_rcnull <- matrix(nrow=39, ncol=20)
+for(r in 1:20){
+  message(paste("Computing r=", r))
+  # Remove taxa with all zeros in both soil and stem data for this radius
+  tmp <- rbind(dnamat, stem.otu$abund[[r]][1:39,])
+  tmp <- tmp[,which(colSums(tmp)!=0)]
+  tmp <- as.matrix(raupcrick(tmp>0, null = "r0"))
+  tmp <- as.matrix(tmp)[1:39, 40:ncol(tmp)]
+  res_rcnull[,r] <- diag(tmp)
+}
+
+boxplot(res_rcnull, ylab="Raup-Crick Null Dissimilarity", xlab="Radius (m)", axes=F)
+axis(1, labels=names(stem.otu$abund), at=1:20)
+axis(2)
+
+
+########################################################
+### LOOK AT DETECTION PROBABILITY VS BASAL AREA IN A NEIGHBORHOOD
+########################################################
+
+## We need to think about how to 
+
+# Choose a certain spatial scale for convenience
+focdat <- stem.otu$ba[[20]]
+
+plot(range(focdat), c(0,1), pch=NA,
+     ylab="Probability of eDNA detection",
+     xlab="Basal area (units?)", xlim=c(0,0.1))
+
+for(sp in 1:ncol(focdat)){
+  y <- (1*(dnamat>0)[,sp])
+  x <- focdat[1:39,sp]
+  mod <- glm(y ~ x, family="binomial")
+  coeffs[sp] <- coef(mod)[2]
+  nd <- data.frame(x=seq(0, max(focdat), length.out=1000))
+  ypred <- predict(mod, nd, type='response')
+  lines(nd$x, ypred, col=ifelse(coeffs[sp]>0, "blue", "red"))
+}
+
+
+#####
+### RELATE DETECTION TO  IS DETECTION SLOPE RELATED TO LEAF TRAITS?
+#####
+
+# One way to do this would be to say, e.g., at what basal area does each species 
+# have a 50% predicted prob of being detected?
+# Then we can correlate those values with traits.
+
+# Another way would be to include traits directly in the regression as interaction term
+
+# We need more thought on this...
+
+library(lme4)
+
+y <- as.vector((1*(dnamat>0)))
+x <- scale(as.vector(as.matrix(focdat[1:39,])))
+tz <- scale(rep(traits$WD, each=39))
+tz[is.na(tz)] <- 0
+
+summary(glm(y ~ x + tz + x*tz, family="binomial"))
+
+
+
+
+
+########################################################
+### RAW ABUNDANCE OF gOTU sequences vs. stems
+########################################################
+focdat <- stem.otu$abund[[5]][1:39,]
+
+focdat.ra <- decostand(focdat, 1, method = "total")
+dnamat.ra <- decostand(dnamat, 1, method = "total")
+
+x <- data.frame(a=unlist(focdat.ra),
+                b=unlist(dnamat.ra))
+par(mfrow=c(2,2), mar=c(4,4,2,1))
+plot(x, 
+     xlab="Stem abundance (25 m radius)",
+     ylab="Number of reads",
+     pch=21, bg=rgb(0,0,0,0.5),
+     main="untransformed"
+)
+# abline(lm((x2$b) ~ (x2$a)), col='blue', lwd=3)
+
+plot(x, 
+     xlab="Stem abundance (25 m radius)",
+     ylab="Number of reads",
+     pch=21, bg=rgb(0,0,0,0.5), log='xy',
+     main="log-log"
+)
+# abline(lm(log10(x2$b) ~ log10(x2$a)), col='blue', lwd=3)
+
+
+# Look across spatial scales
+out <- matrix(nrow=20, ncol=5)
+for(i in seq_along(stem.otu$abund)){
+  focdat <- stem.otu$abund[[i]][201:239,]
+  
+  focdat.ra <- decostand(focdat, 1, method = "total")
+  dnamat.ra <- decostand(dnamat, 1, method = "total")
+  
+  x <- data.frame(a=unlist(focdat.ra), 
+                  b=unlist(dnamat.ra))
+  
+  # x2 <- x[rowSums(apply(x, 2, function(x)x==0))==0,]
+  # s <- summary(lm(log10(x2$b) ~ log10(x2$a)))
+  
+  out[i,1] <- round(s$adj.r.squared, 2)
+  out[i,2] <- round(s$coefficients[2,1], 2)
+  out[i,3] <- round(cor.test(x$b, x$a, use='p')$estimate, 2)
+  out[i,4:5] <- round(cor.test(x$b, x$a)$conf.int, 2)
+}
+
+out <- as.data.frame(out)
+names(out) <- c("r2","coef", "pearsons")
+
+par(mfrow=c(2,1))
+
+plot(seq(5,100,5), out$pearsons, 
+     xlab="Radius (m)", ylab="Pearsons correlation", 
+     ylim=c(-0.01, 0.5), 
+     main="Sequence vs. Stem relative abundance", col='red')
+abline(h=0, lty=2)
+segments(seq(5,100,5), out[,4], 
+         seq(5,100,5), out[,5], col='red')
+
+
+
+# Look across spatial scales
+out <- matrix(nrow=20, ncol=5)
+for(i in seq_along(stem.otu$ba)){
+  focdat <- stem.otu$ba[[i]][201:239,]
+  
+  focdat.ra <- decostand(focdat, 1, method = "total")
+  dnamat.ra <- decostand(dnamat, 1, method = "total")
+  
+  x <- data.frame(a=unlist(focdat.ra), 
+                  b=unlist(dnamat.ra))
+  
+  out[i,1] <- round(s$adj.r.squared, 2)
+  out[i,2] <- round(s$coefficients[2,1], 2)
+  out[i,3] <- round(cor.test(x$b, x$a, use='p')$estimate, 2)
+  out[i,4:5] <- round(cor.test(x$b, x$a)$conf.int, 2)
+}
+
+out <- as.data.frame(out)
+names(out) <- c("r2","coef", "pearsons")
+
+plot(seq(5,100,5), out$pearsons, 
+     xlab="Radius (m)", ylab="Pearsons correlation", 
+     ylim=c(-0.01, 0.5), 
+     main="Sequence vs. Stem relative basal area", col='red')
+abline(h=0, lty=2)
+segments(seq(5,100,5), out[,4], 
+         seq(5,100,5), out[,5], col='red')
+
+
+
+
+
+
+########################################################
+### SPECIES RICHNESS OF DNA AND STEM DATA
+########################################################
+
+
+# Quick plot for a given spatial scale (no rarefaction)
+plot(rowSums(stem.otu$abund[[20]][1:39,]>0),
+     rowSums(dnamat>0))
+
+# Quick plot for a given spatial scale after rarefaction
+plot(rarefy(stem.otu$abund[[20]][1:39,], min(rowSums(stem.otu$abund[[20]][1:39,]))),
+     rarefy(dnamat, min(rowSums(dnamat))))
+
+# Look at correlation coefficients across spatial scales
+corrs <- list()
+corrs_rare <- list()
+
+for(dist in seq_along(stem.otu$abund)){
+  corrs[[dist]] <- cor.test(rowSums(dnamat>0), rowSums(stem.otu$abund[[dist]][1:39,]>0))
+  
+  corrs_rare[[dist]] <- cor.test(rarefy(stem.otu$abund[[dist]][1:39,], 
+                                        min(rowSums(stem.otu$abund[[dist]][1:39,]))),
+                                 rarefy(dnamat, min(rowSums(dnamat))))
+}
+
+par(mfrow=c(1,2))
+
+plot(seq_along(stem.otu$abund), sapply(corrs, function(x) x$estimate), ylim=c(-1,1), axes=F,
+     pch=16, cex=1, xlab="Radius (m)",
+     ylab="Pearson correlation",
+     main="Stem vs. DNA richness [raw]")
+segments(seq_along(stem.otu$abund), y0=sapply(corrs, function(x) x$conf.int[1]),
+         y1=sapply(corrs, function(x) x$conf.int[2]))
+abline(h=0, lty=2)
+axis(1, labels=seq(5, 100, 5), at=1:20)
+axis(2)
+box()
+
+
+plot(seq_along(stem.otu$abund), sapply(corrs_rare, function(x) x$estimate), ylim=c(-1,1), axes=F,
+     pch=16, xlab="Radius (m)",
+     ylab="Pearson correlation",
+     main="Stem vs. DNA richness [rarefied]")
+segments(seq_along(stem.otu$abund), y0=sapply(corrs_rare, function(x) x$conf.int[1]),
+         y1=sapply(corrs_rare, function(x) x$conf.int[2]))
+abline(h=0, lty=2)
+axis(1, labels=seq(5, 100, 5), at=1:20)
+axis(2)
+box()
+
+
+
+
+
+########################################################
+### PRESENCE IN DNA AS A FUNCTION OF DISTANCE TO NEAREST (KNOWN) STEM
+########################################################
+
+x <- unlist(log10(stem.otu$nn[1:39,]))
+y <- as.vector(dnamat.pa)
+
+plot(x, jitter(y, 0.1), pch=16, col=rgb(0,0,0,0.2), 
+     ylab="Presence in DNA",
+     xlab="Distance to nearest individual (log10 m)")
+mod <- glm(y ~ x, family="binomial")
+nd <- data.frame(x=seq(0, max(x), length.out=100))
+ypred <- predict(mod, nd, type='response')
+
+# Overall fit
+lines(nd$x, ypred, col="blue", lwd=3)
+
+# gOTU-specific fits
+coeffs <- vector()
+for(sp in 1:39){
+  xx <- log(unlist(stem.otu$nn[1:39,sp]))
+  yy <- unlist(as.data.frame(1*(dnamat[,sp]>1)))
+  mod <- glm(yy ~ xx, family=binomial)
+  coeffs[sp] <- coef(mod)[2]
+  nd <- data.frame(xx=seq(0, max(x), length.out=100))
+  ypred <- predict(mod, nd, type='response')
+  lines(nd$xx, ypred, col=ifelse(coeffs[sp]>0, rgb(1,0,0,0.4), rgb(0,0,1,0.4)))
+}
+
+
+
