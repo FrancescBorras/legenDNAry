bc4f54aa04060f05b0e7f56d36fb7cafe6861f83
BobMuscarella <bob.muscarella@gmail.com>
2024-05-29 16:11:52 +0200

    Update x.Plot-maps.R

--- Scripts/x.Plot-maps.R
+++ Scripts/x.Plot-maps.R
@@ -4,10 +4,10 @@ library(EDIutils)
 
 
 ### Load data
-dnamat <- readRDS("Processed_data/stem-soil-39pt-data-20240426.RDA")[[1]]
-stem.23 <- readRDS("Processed_data/stem-soil-39pt-data-20240426.RDA")[[2]]
-stem.16 <- readRDS("Processed_data/stem-soil-39pt-data-20240426.RDA")[[4]]
-traits <- readRDS("Processed_data/stem-soil-39pt-data-20240426.RDA")[[3]]
+dnamat <- readRDS("Processed_data/stem-soil-39pt-data-20240510.RDA")[[1]]
+stem.23 <- readRDS("Processed_data/stem-soil-39pt-data-20240510.RDA")[[2]]
+stem.16 <- readRDS("Processed_data/stem-soil-39pt-data-20240510.RDA")[[4]]
+traits <- readRDS("Processed_data/stem-soil-39pt-data-20240510.RDA")[[3]]
 sampxy <- read.csv("Raw_data/LFDP-sample39-coordinates.csv", row.names = 1)
 
 ### Make presence absence matrices
@@ -46,8 +46,8 @@ plot(pts$geometry,
      lwd=scales::rescale(log(pts$DBH), to=c(0.25,0.75)),
      col=cols[as.factor(pts$Mnemonic)])
 plot(b, add=T)
-# axis(1, at=seq(0,300,100))
-# axis(2, at=seq(0,500,100))
+axis(1, at=seq(0,300,100))
+axis(2, at=seq(0,500,100))
 # points(sampxy[,1:2], pch=5, lwd=5)
 
 
@@ -77,3 +77,73 @@ for(r in seq_along(seq(5,100,5))){
 points(150, 150, pch=4, col='red', cex=3, lwd=8)
 
 dev.off()
+
+
+
+
+
+
+
+
+
+### Schematic diagram
+
+pdf("Figures/FigX.map1-for-schematic.pdf")
+
+pts <- st_as_sf(census[census$Status=="alive", c("PX", "PY","DBH", "Mnemonic")],
+                coords = c(1,2))
+
+b <- st_as_sfc(st_bbox(pts))
+
+191
+set.seed(192)
+cols <- sample(colors(distinct=T), length(unique(pts$Mnemonic)))
+
+plot(pts$geometry,
+     cex=scales::rescale((pts$DBH), to=c(0.01, 2)),
+     lwd=scales::rescale(log(pts$DBH), to=c(0.25,1)),
+     col=scales::alpha(cols[as.factor(pts$Mnemonic)], 0.5))
+
+plot(b, add=T)
+axis(1, at=seq(0,300,100))
+axis(2, at=seq(0,500,100))
+points(sampxy[,1:2], pch=4, lwd=2, cex=0.75, col=1)
+# points(sampxy[,1:2], pch=3, col='white', cex=0.5)
+
+dev.off()
+
+
+plot(pts$geometry,
+     xlim=c(150,170), ylim=c(90,110),
+     cex=scales::rescale((pts$DBH), to=c(0.5, 18)),
+     lwd=scales::rescale(log(pts$DBH), to=c(1,5)),
+     col=scales::alpha(cols[as.factor(pts$Mnemonic)], 0.75))
+box()
+axis(1, at=seq(0,300,10))
+axis(2, at=seq(0,500,10))
+points(sampxy[,1:2], pch=4, lwd=2, cex=0.75, col=1)
+
+
+
+plot(pts$geometry,
+     cex=scales::rescale(log(pts$DBH), to=c(0.01,0.8)),
+     lwd=0.5,
+     xlim=c(50,250), ylim=c(50,250),
+     col=cols[as.factor(pts$Mnemonic)])
+box(); axis(1); axis(2)
+
+
+plot(pts$geometry,
+     cex=scales::rescale(log(pts$DBH), to=c(0.01,0.8)),
+     lwd=0.5,
+     xlim=c(50,250), ylim=c(50,250),
+     col='grey')
+box(); axis(1); axis(2)
+
+s <- st_as_sf(data.frame(x=150, y=150),
+              coords = c(1,2))
+
+for(r in seq_along(seq(5,100,5))){
+  plot(sf::st_buffer(s, seq(5,100,5)[r]), add=T, 
+       border=rev(viridis(20))[r], lwd=2)
+}
