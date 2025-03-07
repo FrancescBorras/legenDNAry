291a11b02f93fc8f5b9d42b5e473c1bf7e43710c
BobMuscarella <bob.muscarella@gmail.com>
2025-03-07 20:19:35 +0100

    Update 4.explore-abundance-stuff.R

--- Scripts/4.explore-abundance-stuff.R
+++ Scripts/4.explore-abundance-stuff.R
@@ -44,29 +44,54 @@ ta_prop <- colSums(ta)/npts
 
 ### Plot!
 
-par(mfrow=c(2,2), mar=c(4,4,1,1))
+par(mfcol=c(2,3), mar=c(4,4,1,1))
 
-plot(lfdp23$total_abund, tp_prop, log='',
-     xlab="Total Abundance", ylab="Prop sites where FP @5m",
-     pch=16)
+cp <- viridis_pal(option = "A")(20)[cut(log10(lfdp23$total_ba+0.0001), 20)]
+
+plot(lfdp23$total_abund, tp_prop, log='x',
+     xlab="Total Abundance", ylab="Prop sites where TP @5m",
+     pch=21, bg=cp, lwd=0.5, ylim=c(0,1))
 # If you want to add text labels to know which OTU is which:
- text(lfdp23$total_abund, tp_prop, rownames(lfdp23), cex=0.75)
+ # text(lfdp23$total_abund, tp_prop, rownames(lfdp23), cex=0.75)
+mtext("A", adj=0, line=0.5)
 
-plot(lfdp23$total_abund, fa_prop, log='',
+plot(lfdp23$total_abund, fa_prop, log='x',
      xlab="Total Abundance", ylab="Prop sites where FA @5m",
-     pch=16)
+     pch=21, bg=cp, lwd=0.5, ylim=c(0,1))
+mtext("B", adj=0, line=0.5)
 
-plot(lfdp23$total_abund, fa_prop, log='',
-     xlab="Total Abundance", ylab="Prop sites where TP @ 5m",
-     pch=16)
+plot(lfdp23$total_abund, fp_prop, log='x',
+     xlab="Total Abundance", ylab="Prop sites where FP @ 5m",
+     pch=21, bg=cp, lwd=0.5, ylim=c(0,1))
+mtext("C", adj=0, line=0.5)
 
-plot(lfdp23$total_abund, ta_prop, log='',
+plot(lfdp23$total_abund, ta_prop, log='x',
      xlab="Total Abundance", ylab="Prop sites where TA @5m",
-     pch=16)
+     pch=21, bg=cp, lwd=0.5, ylim=c(0,1))
+mtext("D", adj=0, line=0.5)
+
+### LOOK AT TRUE PRESENCE IN 5M AND HOW THAT RELATES TO TP / FA ETC.
+pres <- colSums(stem.pa.list$`5`[1:npts,])
+
+plot(pres/npts, colSums(tp)/npts, cex=sqrt(lfdp23$total_abund)/30,
+     pch=21, bg=cp, lwd=0.5, xlim=c(0,1), ylim=c(0,1),
+     ylab="Prop sites present @5m", xlab="Prop sites where TP @5m")
+abline(0,1, lty=2)
+mtext("E", adj=0, line=0.5)
+
+plot(pres/npts, colSums(fa)/npts, cex=sqrt(lfdp23$total_abund)/30,
+     pch=21, bg=cp, lwd=0.5, xlim=c(0,1), ylim=c(0,1),
+     ylab="Prop sites present @5m", xlab="Prop sites where FA @5m")
+abline(0,1, lty=2)
+mtext("F", adj=0, line=0.5)
+
+mean(rowSums(stem.pa.list$`25`[1:npts,]))
+
+
 
 
 ### SAME BUT FOR TOTAL BASAL AREA
-par(mfrow=c(2,2), mar=c(4,4,1,1))
+par(mfrow=c(3,2), mar=c(4,4,1,1))
 
 plot(lfdp23$total_ba, tp_prop, log='x',
      xlab="Total Basal Area (m2)", ylab="Prop sites where FP @5m",
@@ -86,22 +111,3 @@ plot(lfdp23$total_ba, ta_prop, log='x',
      xlab="Total Basal Area (m2)", ylab="Prop sites where TA @5m",
      pch=16)
 
-
-### LOOK AT TRUE PRESENCE IN 5M AND HOW THAT RELATES TO TP / FA ETC.
-par(mfrow=c(2,2), mar=c(4,4,1,1))
-
-pres <- colSums(stem.pa.list$`5`[1:npts,])
-
-plot(pres, colSums(tp), cex=sqrt(lfdp23$total_abund)/10)
-abline(0,1)
-
-plot(pres, colSums(fa), cex=sqrt(lfdp23$total_abund)/10)
-abline(0,1)
-
-
-
-
-
-
-
-  
